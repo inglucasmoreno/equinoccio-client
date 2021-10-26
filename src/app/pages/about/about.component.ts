36f199e54132947b17df82baea0e580004fe34f9
Lucas Omar Moreno <ing.lucasmoreno@gmail.com>
2021-10-25 21:22:01 -0300

    Version 2.0

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -12,10 +12,11 @@ export class AboutComponent implements OnInit {
   constructor() { }
   
   ngOnInit(): void {
-    var tl = gsap.timeline({defaults: {duration: 0.1 }});
-    tl.from('.gsap-logo', { y:100, scale:3, opacity:0, duration: 1, rotateY: 360 })
+    var tl = gsap.timeline({defaults: { duration: 0.1 }});
+    tl.from('.gsap-header', { x:200, opacity: 0, duration:0.5 }) 
+      .from('.gsap-logo', { x:-200, opacity: 0, duration:0.5, rotateY: 360})
       .from('.gsap-titulo', { x:100, opacity: 0, duration:0.5 }) 
-      .from('.gsap-contenido', {y:40, opacity: 0, duration: 1}); 
+      .from('.gsap-contenido', { y:100, opacity: 0, duration:0.5 });   
   }  
 
 }
--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -12,7 +12,8 @@ export class ContactComponent implements OnInit {
 
   ngOnInit(): void {
     var tl = gsap.timeline({defaults: { duration: 0.1 }});
-    tl.from('.gsap-logo', { y:200, scale: 5, opacity: 0, duration:0.8, rotateY: 360})
+    tl.from('.gsap-header', { x:200, opacity: 0, duration:0.5 }) 
+      .from('.gsap-logo', { x:-200, opacity: 0, duration:0.5, rotateY: 360})
       .from('.gsap-titulo', { x:100, opacity: 0, duration:0.5 }) 
       .from('.gsap-formulario', { y:100, opacity: 0, duration:0.5 });    
   }
